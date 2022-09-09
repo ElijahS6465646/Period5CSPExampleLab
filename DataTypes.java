c33aae29703f7bb2f3fb42905e4fe320db71dfa7
Elijah Suarez <elijah.suarez@vtsd.com>
2022-09-09 12:27:11 -0400

    My First Commit

---
+++ DataTypes.java
@@ -0,0 +1,12 @@
+//this is a comment (Does not function as code)
+    /*
+    Multi-Line comment syntax
+    nothing in here will be executed (ran)
+     */
+public class DataTypes {
+
+    //Variable syntax: dataType varName = vaule;
+    int elijahFavInt = 19;
+    double snicker = 7.5;
+    boolean likeWater = true;
+}
--- MyFirstProgram.java
+++ MyFirstProgram.java
@@ -1,2 +1,6 @@
-public class MyFirstProgram {ApComputerScience
+public class MyFirstProgram {
+    public static void main(String args[]){
+
+        System.out.print("Hello World");
+    }
 }
